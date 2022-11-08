56b6c46fc4230d6b0edfe059705e078735a0e6d0
Ian <ian.forrester46@gmail.com>
2022-11-07 20:29:40 -0600

    initial commit from Spring init

---
+++ src/main/java/com/ianmcshoeSongr/songrmcshoe/SongrmcshoeApplication.java
@@ -0,0 +1,13 @@
+package com.ianmcshoeSongr.songrmcshoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrmcshoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrmcshoeApplication.class, args);
+	}
+
+}
